e9d08721b430cce7a26ec4484a4f0712da830d57
SharmadDesai <sharmad.desai77@gmail.com>
2017-06-28 23:40:55 +0530

    new ui

---
+++ Assets/Scripts/Ratinglink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ratinglink : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void ratinglink()
+	{
+		Application.OpenURL ("https://play.google.com/store/apps/details?id=com.tiramisugames.colors&hl=en");
+
+	}
+}
